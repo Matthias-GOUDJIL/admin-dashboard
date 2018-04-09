c51d19e24853fdf6fe80e59e621d682529c179bb
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-04-09 23:35:24 +0200

    fixed DOM event

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // variables
-var submitElements = document.getElementsByClassName('btn-submit');
-var listHideElements = document.getElementsByClassName('hide');
-var form = document.querySelectorAll('form');
-var signInElement = document.querySelector('#signIn');
-var signUpElement = document.querySelector('#signUp');
+var submitElements    = document.getElementsByClassName('btn-submit');
+var cancelElements    = document.getElementsByClassName('btn-retour')
+var listHideElements  = document.getElementsByClassName('hide');
+var form              = document.querySelectorAll('form');
+var signInElement     = document.querySelector('#signIn');
+var signUpElement     = document.querySelector('#signUp');
+var modals            = Array.from (document.getElementsByClassName('modal'));
+
+console.log(typeof(Array.from(cancelElements)));
 
 //Affichage du modal signIn:
 document.getElementById('btn-connexion').addEventListener('click', function () {
@@ -16,6 +20,7 @@ document.getElementById('btn-inscription').addEventListener('mousedown', functio
   signUpElement.classList.replace("hidepop","active");
 });
 
+// comportement des boutons submit
 Array.from(submitElements).forEach(function (e) {
   e.addEventListener('mousedown', function () {
     if (signInElement.classList.contains('active')) {
@@ -29,9 +34,25 @@ Array.from(submitElements).forEach(function (e) {
   });
 });
 
- 
+//comportement des bouttons retour
+Array.from(cancelElements).forEach(function (e){
+  e.addEventListener('mousedown', function (e) {
+    modals.forEach(function (e) {
+      // e.preventDefault();
+      console.log(e);
+      if(e.classList.contains('active')){
+        e.classList.replace("active" , "hide");
+        window.location.replace("index.html");
+      }
+    });      
+  });
+});
+
+
+// comportement des submit event
 Array.from(form).forEach(function (e) {
-  e.addEventListener('submit', function () {
+  e.addEventListener('submit', function (e) {
+    e.preventDefault();
     if (signInElement.classList.contains('active')) {
       signInElement.classList.replace("active" , "hidepop");
     }else{
@@ -41,5 +62,5 @@ Array.from(form).forEach(function (e) {
       e.classList.replace("hide", "active");
       console.log(e.classList);
     });
-  }, false);
+  });
 });
